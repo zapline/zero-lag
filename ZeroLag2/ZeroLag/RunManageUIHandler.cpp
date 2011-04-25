ce43d449f3d17947e70ce6ddce034f4568a21c7b
zapline <zapline@zapline-PC>
2011-04-25 20:07:26 +0800

    2011-4-25 ?2.0.7 + ?????? + ????? ??????????

--- ZeroLag2/ZeroLag/RunManageUIHandler.cpp
+++ ZeroLag2/ZeroLag/RunManageUIHandler.cpp
@@ -12,26 +12,7 @@ void CRunManageUIHandler::init(CMainWnd *p)
 	}
 	else
 	{
-		HTREEITEM hItem = RunTree.InsertItem( _T("注册表启动项"), NULL, NULL, KUIMulStatusTree::EM_TVIS_CHECK );
-		RunTree.InsertItem( _T("下属项目1"), hItem, NULL, KUIMulStatusTree::EM_TVIS_CHECK );
-		RunTree.InsertItem( _T("下属项目2"), hItem, NULL, KUIMulStatusTree::EM_TVIS_CHECK );
-		RunTree.InsertItem( _T("下属项目3"), hItem, NULL, KUIMulStatusTree::EM_TVIS_CHECK );
-		RunTree.InsertItem( _T("下属项目4"), hItem, NULL, KUIMulStatusTree::EM_TVIS_CHECK );
-		RunTree.InsertItem( _T("下属项目5"), hItem, NULL, KUIMulStatusTree::EM_TVIS_CHECK );
-
-		hItem = RunTree.InsertItem( _T("目录启动项"), NULL, NULL, KUIMulStatusTree::EM_TVIS_UNCHECK );
-		RunTree.InsertItem( _T("下属项目1"), hItem, NULL, KUIMulStatusTree::EM_TVIS_CHECK );
-		RunTree.InsertItem( _T("下属项目2"), hItem, NULL, KUIMulStatusTree::EM_TVIS_UNCHECK );
-		RunTree.InsertItem( _T("下属项目3"), hItem, NULL, KUIMulStatusTree::EM_TVIS_CHECK );
-		RunTree.InsertItem( _T("下属项目4"), hItem, NULL, KUIMulStatusTree::EM_TVIS_UNCHECK );
-		RunTree.InsertItem( _T("下属项目5"), hItem, NULL, KUIMulStatusTree::EM_TVIS_UNCHECK );
-
-		hItem = RunTree.InsertItem( _T("服务启动项"), NULL, NULL, KUIMulStatusTree::EM_TVIS_UNCHECK );
-		RunTree.InsertItem( _T("下属项目1"), hItem, NULL, KUIMulStatusTree::EM_TVIS_UNCHECK );
-		RunTree.InsertItem( _T("下属项目2"), hItem, NULL, KUIMulStatusTree::EM_TVIS_UNCHECK );
-		RunTree.InsertItem( _T("下属项目3"), hItem, NULL, KUIMulStatusTree::EM_TVIS_UNCHECK );
-		RunTree.InsertItem( _T("下属项目4"), hItem, NULL, KUIMulStatusTree::EM_TVIS_UNCHECK );
-		RunTree.InsertItem( _T("下属项目5"), hItem, NULL, KUIMulStatusTree::EM_TVIS_UNCHECK );
+		scan();
 	}
 }
 
@@ -44,4 +25,81 @@ CRunManageUIHandler::CRunManageUIHandler(void)
 
 CRunManageUIHandler::~CRunManageUIHandler(void)
 {
+}
+
+void CRunManageUIHandler::scan()
+{
+	RunTree.DeleteAllItems();
+	pMain->SetItemVisible(3101,FALSE);
+
+	//0为全部扫描完毕
+	ThreadStatus = 2;
+	ht0 = RunTree.InsertItem( _T("注册表启动项"), NULL, NULL, KUIMulStatusTree::EM_TVIS_UNCHECK );
+	ht1 = RunTree.InsertItem( _T("文件夹启动项"), NULL, NULL, KUIMulStatusTree::EM_TVIS_UNCHECK );
+
+	RunManage = new CRunManage();
+	RunManage->init();
+	hThread1 = CreateThread(0,0,(LPTHREAD_START_ROUTINE)ScanRegRunThread,this,0,0);
+	hThread2 = CreateThread(0,0,(LPTHREAD_START_ROUTINE)ScanDirRunThread,this,0,0);
+}
+
+void CRunManageUIHandler::OnRunScan()
+{
+	scan();
+
+}
+void ScanRegRunThread(LPVOID param)
+{
+	CRunManageUIHandler *p = (CRunManageUIHandler *)param;
+
+	CString temp;
+
+	int count = p->RunManage->RegEnumer->GetCount();
+	if (count == 0)
+	{
+		p->RunTree.InsertItem( _T("无开机启动项"),  p->ht0, NULL, KUIMulStatusTree::EM_TVIS_UNCHECK );
+	}
+	else
+	{
+		for (int i=0;i<count;i++)
+		{
+			temp.Format("%s", p->RunManage->RegEnumer->GetItem(i).strPath);
+			p->RunTree.InsertItem(temp,  p->ht0, NULL, KUIMulStatusTree::EM_TVIS_UNCHECK );
+		}
+	}
+
+	if (--(p->ThreadStatus) == 0)
+	{
+		p->pMain->SetItemVisible(3101,TRUE);
+		delete p->RunManage;
+	}
+	CloseHandle(p->hThread1);
+}
+
+void ScanDirRunThread(LPVOID param)
+{
+	CRunManageUIHandler *p = (CRunManageUIHandler *)param;
+
+	CString temp;
+
+	int count = p->RunManage->DirEnumer->GetCount();
+	if (count == 0)
+	{
+		p->RunTree.InsertItem( _T("无开机启动项"),  p->ht1, NULL, KUIMulStatusTree::EM_TVIS_UNCHECK );
+	}
+	else
+	{
+		for (int i=0;i<count;i++)
+		{
+			temp.Format("%s", p->RunManage->DirEnumer->GetItem(i).strParam);
+			p->RunTree.InsertItem(temp,  p->ht1, NULL, KUIMulStatusTree::EM_TVIS_UNCHECK );
+		}
+	}
+
+	if (--(p->ThreadStatus) == 0)
+	{
+		p->pMain->SetItemVisible(3101,TRUE);
+		delete p->RunManage;
+	}
+	CloseHandle(p->hThread2);
 }
--- ZeroLag2/ZeroLag/RunManageUIHandler.h
+++ ZeroLag2/ZeroLag/RunManageUIHandler.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "stdafx.h"
+#include "RunManage.h"
 
 class CMainWnd;
 
@@ -7,11 +8,25 @@ class CRunManageUIHandler
 {		   
 public:
 	void init(CMainWnd *p);
+	void OnRunScan();
 	CMainWnd *pMain;
 
 	KUIMulStatusTree RunTree;
+	friend void ScanRegRunThread(LPVOID param);
+	friend void ScanDirRunThread(LPVOID param);
+
+	void scan();
+
+	CString GetFilenameInPathname(CString pathname);
+
+	HANDLE hThread1,hThread2;
+	HTREEITEM ht0,ht1;
+	int ThreadStatus;
+
+	CRunManage *RunManage;
 
 	KUI_NOTIFY_MAP(IDC_RICHVIEW_WIN)
+		KUI_NOTIFY_ID_COMMAND(3101, OnRunScan)
 		KUI_NOTIFY_MAP_END()
 
 		BEGIN_MSG_MAP_EX(CRunManageUIHandler)
