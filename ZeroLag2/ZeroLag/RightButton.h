eeb2df0bce56b1d046670bbdf2e2c4628622aa8e
Administrator <Administrator@PC-200201010241>
2011-04-29 21:53:33 +0800

    ????????

---
+++ ZeroLag2/ZeroLag/RightButton.h
@@ -0,0 +1,14 @@
+#pragma once
+
+class CRightButton
+{
+public:
+	CRightButton(void);
+public:
+	~CRightButton(void);
+
+	static int AddSystemRecycle();
+	static int DelSystemRecycle();
+	static int AddSystemCutTo();
+	static int DelSystemCutTo();
+};
--- ZeroLag2/ZeroLag/RightButtonUIHandler.cpp
+++ ZeroLag2/ZeroLag/RightButtonUIHandler.cpp
@@ -11,9 +11,35 @@ void CRightButtonUIHandler::init(CMainWnd *p)
 
 CRightButtonUIHandler::CRightButtonUIHandler(void)
 {
+	
 }
 
 
 CRightButtonUIHandler::~CRightButtonUIHandler(void)
 {
+}
+
+void CRightButtonUIHandler::OnAddSysRecycle()
+{
+	CRightButton::AddSystemRecycle();
+	MessageBox(0,"添加成功!","",MB_OK);
+	
+}
+
+void CRightButtonUIHandler::OnDelSysRecycle()
+{
+	CRightButton::DelSystemRecycle();
+	MessageBox(0,"删除成功!","",MB_OK);
+}
+
+void CRightButtonUIHandler::OnAddCutTo()
+{
+	CRightButton::AddSystemCutTo();
+	MessageBox(0,"添加成功!","",MB_OK);
+}
+
+void CRightButtonUIHandler::OnDelCutTo()
+{
+	CRightButton::DelSystemCutTo();
+	MessageBox(0,"删除成功!","",MB_OK);
 }
