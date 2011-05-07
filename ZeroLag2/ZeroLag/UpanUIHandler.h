04f2b30c9ca227e5c85d11ed44e9990a97bc74c2
zapline <zapline@zapline-PC>
2011-05-07 20:41:44 +0800

    2011-5-7 ?2.0.9 + ?????????? + ??U??????? + autorun???? * ??kscres??????? ???CKuiMsgBox????

--- ZeroLag2/ZeroLag/UpanUIHandler.h
+++ ZeroLag2/ZeroLag/UpanUIHandler.h
@@ -1,5 +1,6 @@
 #pragma once	  
 #include "stdafx.h"
+#include "Upan.h"
 
 class CMainWnd;
 
@@ -8,15 +9,25 @@ class CUpanUIHandler
 public:
 	void init(CMainWnd *p);
 	CMainWnd *pMain;
+	CUpan Upan;
+
+	void OnCreateAutorun();
+	void OnKillHide();
+	LRESULT OnList1(DWORD whParam, DWORD wlParam, HWND lParam,BOOL bHandled);
+	LRESULT OnList2(DWORD whParam, DWORD wlParam, HWND lParam,BOOL bHandled);
 
 	CComboBox m_DriveList,m_UpanList;
 
 	KUI_NOTIFY_MAP(IDC_RICHVIEW_WIN)
-		KUI_NOTIFY_MAP_END()
+		KUI_NOTIFY_ID_COMMAND(7105 ,OnCreateAutorun)
+		KUI_NOTIFY_ID_COMMAND(7106 ,OnKillHide)
+	KUI_NOTIFY_MAP_END()
 
-		BEGIN_MSG_MAP_EX(CUpanUIHandler)
+	BEGIN_MSG_MAP_EX(CUpanUIHandler)
 		MSG_KUI_NOTIFY(IDC_RICHVIEW_WIN)
-		END_MSG_MAP()
+		COMMAND_HANDLER(7001, CBN_DROPDOWN, OnList1)
+		COMMAND_HANDLER(7002, CBN_DROPDOWN, OnList2)
+	END_MSG_MAP()
 public:
 	CUpanUIHandler(void);
 	~CUpanUIHandler(void);
